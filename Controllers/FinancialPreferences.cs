bc4db028b684df0809c62068e402f62526df601d
raytzou <raytzou@outlook.com>
2025-05-12 20:18:23 +0800

    init FinancialPreferences and View

---
+++ Controllers/FinancialPreferences.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialPreferences.Controllers
+{
+    public class FinancialPreferences : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
