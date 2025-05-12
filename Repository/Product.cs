b7b8a6f940c56994a8605c2f5ae42577629ab856
raytzou <raytzou@outlook.com>
2025-05-13 00:09:08 +0800

    Add IProduct interface and Product class for data access
    A new `IProduct` interface was introduced in the `Repository.Interface`
    namespace, defining a `GetProducts()` method to retrieve product data.
    
    The `Product` class in the `Repository` namespace implements this
    interface. It connects to a SQL Server database, executes the
    `sp_GetAllProducts` stored procedure, maps the results to a list of
    `Common.Product` objects using `SqlDataReader`, and returns the list.
    
    Namespaces used include `Microsoft.Data.SqlClient`, `System.Data`, and
    `Repository.Interface`.

---
+++ Repository/Product.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using Repository.Interface;
+using System.Data;
+
+namespace Repository
+{
+    public class Product : IProduct
+    {
+        public IEnumerable<Common.Product> GetProducts()
+        {
+            var connectionString = "Server=localhost;Database=Financial;TrustServerCertificate=True;User Id=demo;Password=your_password;";
+            var products = new List<Common.Product>();
+
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("sp_GetAllProducts", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    // get index for each column, no need to call GetOrdinal multiple times
+                    var idIdx = reader.GetOrdinal("ProductId");
+                    var nameIdx = reader.GetOrdinal("ProductName");
+                    var priceIdx = reader.GetOrdinal("Price");
+                    var feeIdx = reader.GetOrdinal("FeeRate");
+
+                    while (reader.Read())
+                    {
+                        var product = new Common.Product
+                        {
+                            ProductId = reader.GetGuid(idIdx),
+                            ProductName = reader.GetString(nameIdx),
+                            Price = reader.GetDecimal(priceIdx),
+                            FeeRate = reader.GetDecimal(feeIdx)
+                        };
+                        products.Add(product);
+                    }
+                };
+            };
+
+            return products;
+        }
+    }
+}
