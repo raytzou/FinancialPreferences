9fc97e0cd96f7e0b1db43b4e8988b21077f28b9b
raytzou <raytzou@outlook.com>
2025-05-13 02:02:37 +0800

    Add User common model

---
+++ Common/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string AccountNumber { get; set; }
+    }
+}
