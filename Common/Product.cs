ad260f9f0f268264b6ce68e2949774adf564df9a
raytzou <raytzou@outlook.com>
2025-05-13 00:04:20 +0800

    Add Product common model(DTO)

---
+++ Common/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal FeeRate { get; set; }
+    }
+}
