14063811bcc1cd59601706d56aaa2fa92aafd356
raytzou <raytzou@outlook.com>
2025-05-13 21:46:10 +0800

    Add UserPreference Common model

---
+++ Common/UserPreference.cs
@@ -0,0 +1,13 @@
+﻿namespace Common
+{
+    public class UserPreference
+    {
+        public Guid PreferenceId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public int OrderQuantity { get; set; }
+        public string AccountNumber { get; set; }
+        public int TotalAmount { get; set; }
+        public decimal TotalFee { get; set; }
+    }
+}
