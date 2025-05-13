7cb0c2ab109d8b2f8e2014036573844729fcf2fd
raytzou <raytzou@outlook.com>
2025-05-13 23:36:49 +0800

    Add FinancialPreferencesService and interface skeleton
    Introduce FinancialPreferencesService class in the
    BussinessLogic.Services namespace, implementing the
    IFinancialPreferencesService interface. Add the
    IFinancialPreferencesService interface in the
    BussinessLogic.Services.Interfaces namespace, defining a
    Validate method for UserPreference objects. Include necessary
    using directives for proper namespace references. Note:
    Validate method is not yet implemented.

---
+++ Service/Services/FinancialPreferencesService.cs
@@ -0,0 +1,13 @@
+﻿using BussinessLogic.Services.Interfaces;
+using Common.Models;
+
+namespace BussinessLogic.Services
+{
+    public class FinancialPreferencesService : IFinancialPreferencesService
+    {
+        public void Validate(UserPreference userPreference)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Service/Services/Interfaces/IFinancialPreferencesService.cs
@@ -0,0 +1,7 @@
+﻿namespace BussinessLogic.Services.Interfaces
+{
+    public interface IFinancialPreferencesService
+    {
+        void Validate(Common.Models.UserPreference userPreference);
+    }
+}
