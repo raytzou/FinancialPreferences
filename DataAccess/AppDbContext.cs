427718ba06a2866ca967802b75ed91381e81645f
raytzou <raytzou@outlook.com>
2025-09-28 20:15:38 +0800

    feat: add DbContext

---
+++ DataAccess/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<House> Houses { get; set; } = null!;
+    }
+}
