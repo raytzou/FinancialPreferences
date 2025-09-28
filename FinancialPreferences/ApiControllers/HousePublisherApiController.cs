97d771968e33573b02fb0b24dd1de2067188a61c
raytzou <raytzou@outlook.com>
2025-09-29 00:36:57 +0800

    feat: add House Publisher API Controller

---
+++ FinancialPreferences/ApiControllers/HousePublisherApiController.cs
@@ -0,0 +1,13 @@
+﻿using Common.Models;
+using System.Web.Http;
+
+namespace FinancialPreferences.ApiControllers
+{
+    public class HousePublisherApiController : ApiController
+    {
+        public List<House> GetAllHouses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
