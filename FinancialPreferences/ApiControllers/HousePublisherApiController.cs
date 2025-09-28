4413e0e5a6265c9e36e17c0b0369eae0892c0547
raytzou <raytzou@outlook.com>
2025-09-29 00:37:58 +0800

    chore: service DI

--- FinancialPreferences/ApiControllers/HousePublisherApiController.cs
+++ FinancialPreferences/ApiControllers/HousePublisherApiController.cs
@@ -1,10 +1,18 @@
-﻿using Common.Models;
+﻿using BusinessLogic.Services.Interfaces;
+using Common.Models;
 using System.Web.Http;
 
 namespace FinancialPreferences.ApiControllers
 {
     public class HousePublisherApiController : ApiController
     {
+        private IHousePublisherService _service;
+
+        public HousePublisherApiController(IHousePublisherService service)
+        {
+            _service = service;
+        }
+
         public List<House> GetAllHouses()
         {
             throw new NotImplementedException();
